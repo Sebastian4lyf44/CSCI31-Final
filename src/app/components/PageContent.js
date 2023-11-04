d720c107e67456a6ea78630df9f78ed3efbe9a77
Sebastian Stimpson <Stimpsonsebastian094@icloud.com>
2023-11-04 12:36:32 -0700

    first commit

---
+++ src/app/components/PageContent.js
@@ -0,0 +1,5 @@
+const defaultContent = `Bob was convinced his cat had a job. Every morning, Mr. Whiskers would leave the house at 8 a.m., only to return at 5 p.m. with a mysterious air about him. Bob even once found a tiny briefcase filled with small mouse currency and a business card that read, "Mr. Whiskers, CEO of CatCorp." The corporate world was a jungle, but it seemed even the urban kitties had found their way in!`
+
+export default function PageContent({content = defaultContent}) {
+    return<main className="bg-hray-100 rounded-md p-6">{content}</main>
+}
