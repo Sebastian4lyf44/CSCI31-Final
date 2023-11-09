9868a764c68647ef8beffbd9cf939e0a21c7dc91
Sebastian Stimpson <Stimpsonsebastian094@icloud.com>
2023-11-08 20:40:26 -0800

    working example

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,11 @@ import Navbar from './components/Navbar'
 import PageTitle from './components/PageTitle'
 import PageContent from './components/PageContent'
 export default function Home() {
-  return
-<div>
-  <Navbar/>
-  <div className="m-12">
-  <PageTitle title="Home" />
-  <PageContent />
-  </div>
-  </div>
+  return (<div>
+    <Navbar/>
+    <div className="m-12">
+    <PageTitle title="Home" />
+    <PageContent />
+    </div>
+    </div>);
 }
